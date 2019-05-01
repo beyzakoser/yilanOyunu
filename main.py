ea1ee2e04ad129837719baa970697ba16658f091
beyzakoser <b.koser@hotmail.com>
2019-05-01 13:35:26 +0300

    ekran gecisi

--- main.py
+++ main.py
@@ -1,87 +1,56 @@
 import pygame
 
-class yilan():
-    def __init__(self,screen):
-        self.x=0
-        self.y=0
-        self.imageOrder=0 #görüntü sırası
-        width=screen.get_width()
-        height=screen.get_height()
-        self.rectange=[200,int(height/2),int(width/10),int(height/10)]
-
-
-    def draw(self,screen,mx,my):
-        self.imageOrder = self.imageOrder % 3 + 1
-        self.image=pygame.image.load("images/kafa("+str(self.imageOrder)+").png")
-        self.rectange[0]=self.rectange[0]+mx*3
-        self.rectange[1]=self.rectange[1]+my*3
-        self.image= pygame.transform.scale(self.image, (self.rectange[2],self.rectange[3]))
-        screen.blit(self.image, self.rectange)
-
-    def drawYukari(self, screen, mx, my):
-        self.imageOrder = self.imageOrder % 3 + 1
-        self.image = pygame.image.load("images/kafaY(" + str(self.imageOrder) + ").png")
-        self.rectange[0] = self.rectange[0] + mx * 3
-        self.rectange[1] = self.rectange[1] + my * 3
-        self.image = pygame.transform.scale(self.image, (self.rectange[2], self.rectange[3]))
-        screen.blit(self.image, self.rectange)
-
-
 pygame.init()
 
-gameDisplay = pygame.display.set_mode((870,650))
-pygame.display.set_caption('Snake')
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
 
+gameDisplay = pygame.display.set_mode((800, 600)) #ekran boyutu
+pygame.display.set_caption('Snake Game')
 
-crashed = False
-clock = pygame.time.Clock() #zamanlama için bir clock tanımlıyoruz
+gameExit = False
 
-white = (255, 255,255)
-black = (0,0,0)
-gameDisplay.fill(white)
+yilan_x = 300
+yilan_y = 300
 
-myimage = pygame.image.load("images/acikKahve.jpg") #beyaz olanda güzel oldu sonra tekrar bak
-backGraundImage= pygame.transform.scale(myimage, (gameDisplay.get_width(), gameDisplay.get_height()))
-gamePlane=yilan(gameDisplay)
-mx=0
-my=0
-while not crashed:
-    for event in pygame.event.get():  # oluşmuş olan olayları tarıyoruz
-        if event.type == pygame.QUIT:  # olay tipine göre karar mekanizmalarını kuruyoruz
-            crashed = True  #  döngüyü kırar
+mx = 0
+my = 0
+
+clock = pygame.time.Clock()
+
+while not gameExit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameExit = True
         if event.type == pygame.KEYDOWN:
-            gamePlane.drawYukari(gameDisplay, mx, my)
-            if event.key == pygame.K_UP:
-                gamePlane.drawYukari(gameDisplay, mx, my)
-                my=-1
-            if event.key == pygame.K_DOWN:
-                my=1
             if event.key == pygame.K_LEFT:
-                mx=-1
-            if event.key == pygame.K_RIGHT:
-                mx=1
-        if event.type == pygame.KEYUP:
-            gamePlane.drawYukari(gameDisplay, mx, my)
-            if event.key == pygame.K_UP:
-                gamePlane.drawYukari(gameDisplay, mx, my)
+                mx  = -5
+                my  = 0
+            elif event.key == pygame.K_RIGHT:
+                mx  = 5
+                my  = 0
+            elif event.key == pygame.K_UP:
+                my  = -5
+                mx  = 0
+            elif event.key == pygame.K_DOWN:
+                my = 5
+                mx  = 0
 
-                my=0
+    yilan_x += mx
+    yilan_y += my
 
-            if event.key == pygame.K_DOWN:
-                my=0
-            if event.key == pygame.K_LEFT:
-                mx=0
-            if event.key == pygame.K_RIGHT:
-                mx=0
+#bu kısım diğer ekrandan çıkabilmesi için
+    if yilan_y >= 600: yilan_y=0
+    elif yilan_y <0: yilan_y=599
+    if yilan_x>=800: yilan_x=0
+    elif yilan_x<0 : yilan_x=799
 
-    gameDisplay.blit(backGraundImage, (0, 0))
-    mouse_position = pygame.mouse.get_pos()
-    gamePlane.draw(gameDisplay, mx, my)
+    gameDisplay.fill(white)
+    pygame.draw.rect(gameDisplay, black, [yilan_x, yilan_y, 20, 20])
+    pygame.display.update()
 
-
-
-    pygame.display.update()  # her döngüde ekranı tekrar çizdiriyoruz
-    clock.tick(20)  #burada saniyede 60 framelik bir yenilenme olacak demektir.
+    clock.tick(30)
 
 pygame.quit()
 quit()
